ec0f0adfb79500553bdd8335cabae9133d439cda
Abadie Moran <moran.abadie@free.fr>
2018-06-23 10:13:01 +0200

    Update settings.py

--- SmartHome/settings.py
+++ SmartHome/settings.py
@@ -34,8 +34,7 @@ except:
         sys.exit('error read key')
 
 SECRET_KEY = sec
-#ID_USER_WEATHER = os.environ['ID_USER_WEATHER']
-ID_USER_WEATHER = "f0b649bd38a937e091091572ae3b2d62"
+ID_USER_WEATHER = os.environ['ID_USER_WEATHER']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
